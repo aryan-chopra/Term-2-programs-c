b61ff98200c3c59b92d63c7951ef32fbb2ce567b
Aryan Chopra <122869021+aryan-chopra@users.noreply.github.com>
2023-01-27 00:16:32 +0530

    Update prog2_addTwoNumbers.c

--- prog2_addTwoNumbers.c
+++ prog2_addTwoNumbers.c
@@ -1,19 +1,4 @@
-//// Write a program to show the use to input (Scanf)/ output (Printf) statements and block structures of C program by highlighting the features of stdio.h
-
-#include <stdio.h>
-
-int main(void) {
-
-    int number;
-
-    //Getting a user input, demonstrating the use of 'scanf'
-    printf("Enter a number: ");
-    scanf("%d", &number);
-
-    //Printing the number input by the user, demonstrating the use of 'printf'
-    printf("The number you entered was: %d\n", number);
-    return 0;
-}
+// Write a program to show the use to input (Scanf)/ output (Printf) statements and block structures of C program by highlighting the features of stdio.h
 
 #include <stdio.h>
 
@@ -30,4 +15,6 @@ int main(void)
     //Adding the numbers and printing them
     int sum = firstNumber + secondNumber;
     printf("Sum: %i", sum);
+    
+    return 0;
 }
