1b6fc33cd596f344c9cd62b37b65655c566459e6
Aryan Chopra <aryan.chopra1211@gmail.com>
2023-02-02 11:05:05 +0530

    program 6(a) written

---
+++ prog6_swap_temp.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int firstNumber, secondNumber;
+
+    printf("Enter first number: ");
+    scanf("%d", &firstNumber);
+    printf("Enter second number: ");
+    scanf("%d", &secondNumber);
+
+    int temp = secondNumber;
+    secondNumber = firstNumber;
+    firstNumber = temp;
+
+    printf("First number after swapping: %i\n", firstNumber);
+    printf("Second number after swapping: %i\n", secondNumber);
+
+    return 0;
+}
