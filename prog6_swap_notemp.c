df5d940f4bf43939c5b73d7f0ff53df00eedb498
Aryan Chopra <aryan.chopra1211@gmail.com>
2023-02-02 11:10:32 +0530

    written program 6(b)

---
+++ prog6_swap_notemp.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int firstNumber, secondNumber;
+
+    printf("Enter first number: ");
+    scanf("%d", &firstNumber);
+    printf("Enter the second number: ");
+    scanf("%d", &secondNumber);
+
+    firstNumber = firstNumber + secondNumber;
+    secondNumber = firstNumber - secondNumber;
+    firstNumber = firstNumber - secondNumber;
+
+    printf("First number after swapping: %i\n", firstNumber);
+    printf("Second number after swapping: %i\n", secondNumber);
+
+    return 0;
+}
